feac0c35b1da54d5a9a53418d924650808339968
Kei Kamikawa <Code-Hex@users.noreply.github.com>
2024-10-26 23:04:55 +0900

    upgrade cross make version to 0.9.10

--- build.sh
+++ build.sh
@@ -5,21 +5,31 @@ set -eu
 
 srcdir="$PWD"
 target_arch="aarch64 x86_64 armv7l"
-cross_make_ver=0.9.9
+cross_make_ver=0.9.10
 cross_make_dir="$srcdir/musl-cross-make-${cross_make_ver}"
 
+_musl_or_abihf() {
+        local arch="$1"
+        if [ $arch == "armv7l" ]; then
+                echo musleabihf
+        else
+                echo musl
+        fi
+}
+
 _musl_path() {
-	local arch="$1"
-	local machine_arch="$(uname -m)"
-	if [ $arch == $machine_arch ]; then
-		echo $arch-linux-musl-native
-	else
-		echo $arch-linux-musl-cross
-	fi
+        local arch="$1"
+        local machine_arch="$(uname -m)"
+        local musl="$(_musl_or_abihf $arch)"
+        if [ $arch == $machine_arch ]; then
+                echo $arch-linux-$musl-native
+        else
+                echo $arch-linux-$musl-cross
+        fi
 }
 
 if [ ! -f v${cross_make_ver}.tar.gz ]; then
-	curl -LO "https://github.com/richfelker/musl-cross-make/archive/v${cross_make_ver}.tar.gz"
+    curl -LO "https://github.com/richfelker/musl-cross-make/archive/v${cross_make_ver}.tar.gz"
 fi
 
 tar xvf "v${cross_make_ver}.tar.gz"
@@ -30,11 +40,10 @@ for _arch in $target_arch; do
     if [ "$_arch" == "armv7l" ]; then
         make install TARGET="armv7l-linux-musleabihf" \
             GCC_CONFIG+="--with-arch=armv7-a --with-fpu=vfpv3-d16 --with-float=hard"
-        dirname=$(_musl_path armv7l)
     else
         make install TARGET="${_arch}-linux-musl"
-        dirname=$(_musl_path $_arch)
     fi
+    dirname=$(_musl_path $_arch)
     mv output $dirname
     tar czf "${dirname}.tar.gz" $dirname
     mv "${dirname}.tar.gz" $srcdir
--- config.mak
+++ config.mak
@@ -1,9 +1,2 @@
 # Drop some features for faster and smaller builds
 COMMON_CONFIG += --disable-nls
-GCC_CONFIG += --disable-libquadmath --disable-decimal-float
-GCC_CONFIG += --disable-libitm --disable-fixed-point
-
-# Explicitly enable libisl support to avoid opportunistic linking
-ISL_VER = 0.21
-ISL_SITE = https://downloads.sourceforge.net/project/libisl
-
